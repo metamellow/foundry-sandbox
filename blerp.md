0441513bf6d3a9baeb76b057b5360f1ae5237568
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-04-11 16:08:48 +0700

    all files added, time to test

--- blerp.md
+++ blerp.md
@@ -1,11 +1,15 @@
 # DEV NOTES
 - 
+- airdrop list: https://docs.google.com/spreadsheets/d/1utck9-9MXnCJFf329RT2UoMNhQqgVd2wXyJhJJnl7ZY/
 
-# Token Testing
-- 
+# [1] Token Testing
+- forge test --fork-url https://polygon-mainnet.g.alchemy.com/v2/elpiyNU3HOchYaeMMpCteXolAFqJYTEi --match-path test/bonToken.t.sol -vvv
 
-# Staking Testing
-- 
+# [2] Staking Testing
+- forge test --fork-url https://polygon-mainnet.g.alchemy.com/v2/elpiyNU3HOchYaeMMpCteXolAFqJYTEi --match-path test/bonTokenStaking.t.sol -vvv
 
-# Bridging Testing
-- 
+# [3] Bridging Testing
+- forge test --fork-url https://polygon-mainnet.g.alchemy.com/v2/elpiyNU3HOchYaeMMpCteXolAFqJYTEi --match-path test/bonExchange.t.sol -vvv
+
+# [4] NFT Testing
+- forge test --fork-url https://polygon-mainnet.g.alchemy.com/v2/elpiyNU3HOchYaeMMpCteXolAFqJYTEi --match-path test/bonNFT.t.sol -vvv
---
+++ test/bonNFT.t.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+/*
+NOTES:
+- for more testing ideas, see: https://github.com/s0crates-eth/foundry_sandbox/blob/main/test/NFT.t.sol
+*/
+
+
+import "forge-std/Test.sol";
+import "../src/bonNFT.sol";
+
+contract contractTest is Test {
+    bonNFT public contractTested;
+    address public RVLT = 0xf0f9D895aCa5c8678f706FB8216fa22957685A13;
+
+    function setUp() public{
+        // wallets setup
+        address user = address(69);
+        vm.startPrank(user);
+        address[] memory testAddresses = new address[](3);
+            testAddresses[0] = address(70);
+            testAddresses[1] = address(71);
+            testAddresses[2] = address(72);
+
+        // token setup
+        vm.deal(user, 1_000_000 ether);
+        deal(RVLT, user, 1_000_000_000_000 ether);
+        bool success = IERC20(RVLT).approve(
+            0x8fA079a96cE08F6E8A53c1C00566c434b248BFC4, 
+            115792089237316195423570985008687907853269984665640564039457584007913129639935);
+        require(success, "Approve failed");
+        
+        // contract setup
+        contractTested = new bonNFT(testAddresses);
+    }
+
+    function test_initialState() public{
+        assertEq(contractTested.totalSupply(), 23);
+    }
+
+    function test_nftTokenTransferAndMinting() public{
+        contractTested.flipSaleState();
+        uint beforeErcBal = IERC20(RVLT).balanceOf(address(69));
+        contractTested.mint{value: 1 ether}(2);
+        uint afterErcBal = IERC20(RVLT).balanceOf(address(69));
+        assertFalse(beforeErcBal == afterErcBal);
+        assertEq(contractTested.totalSupply(), 25);
+    }
+
+    function testFail_mintingGasNotEnough() public{
+        contractTested.flipSaleState();
+        contractTested.mint{value: 1}(2);
+    }
+
+    function testFail_mintButSaleNotActive() public{
+        contractTested.mint{value: 1 ether}(2);
+    }
+    
+    function testFail_mintSurpassMax() public{
+        contractTested.flipSaleState();
+        contractTested.mint(11);
+    }
+
+    function test_withdrawAll() public{
+        contractTested.flipSaleState();
+        contractTested.mint{value: 1 ether}(2);
+        uint b4Wallet70erc = IERC20(RVLT).balanceOf(address(70));
+        uint b4Wallet71erc = IERC20(RVLT).balanceOf(address(71));
+        uint b4Wallet72erc = IERC20(RVLT).balanceOf(address(72));
+        uint b4Wallet70gas = address(70).balance;
+        uint b4Wallet71gas = address(71).balance;
+        uint b4Wallet72gas = address(72).balance;
+        contractTested.withdrawAll(address(70), address(71), address(72));
+        uint AftWallet70erc = IERC20(RVLT).balanceOf(address(70));
+        uint AftWallet71erc = IERC20(RVLT).balanceOf(address(71));
+        uint AftWallet72erc = IERC20(RVLT).balanceOf(address(72));
+        uint AftWallet70gas = address(70).balance;
+        uint AftWallet71gas = address(71).balance;
+        uint AftWallet72gas = address(72).balance;
+        assertTrue(b4Wallet70erc == 0);
+        assertFalse(AftWallet70erc == 0);
+        assertTrue(b4Wallet70gas == 0);
+        assertFalse(AftWallet70gas == 0);
+        assertFalse(b4Wallet70erc == AftWallet70erc);
+        assertFalse(b4Wallet70gas == AftWallet70gas);
+        assertFalse(b4Wallet71erc == AftWallet71erc);
+        assertFalse(b4Wallet71gas == AftWallet71gas);
+        assertFalse(b4Wallet72erc == AftWallet72erc);
+        assertFalse(b4Wallet72gas == AftWallet72gas);
+    }
+
+}
