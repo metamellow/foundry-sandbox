8fc868efb31c6f866de4c2e645758bf008abcd45
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-05-17 18:08:09 +0700

    Create command center.sol

---
+++ 0xTO_DO_LIST/command center.sol
@@ -0,0 +1,1 @@
+an onlyOwner master controller, one which calls many of the manual duties that I will have to do on a regular basis like drain all exchanged and brisge them and then stock their twin
