abb8f26594fb443a3a61841b604c68791007a4f2
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-05-17 20:02:22 +0700

    Update command center.sol

--- 0xTO_DO_LIST/command center.sol
+++ 0xTO_DO_LIST/command center.sol
@@ -1,1 +1,4 @@
 an onlyOwner master controller, one which calls many of the manual duties that I will have to do on a regular basis like drain all exchanged and brisge them and then stock their twin
+
+
+use something like Chainlink or Gelato to auto trigger something lile everyfull moon
