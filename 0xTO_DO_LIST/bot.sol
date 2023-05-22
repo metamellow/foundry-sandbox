b31d892edfb877580212f78a0ed747c262b50582
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-05-22 07:36:25 +0700

    Update bot.sol

--- 0xTO_DO_LIST/bot.sol
+++ 0xTO_DO_LIST/bot.sol
@@ -1,313 +1,5 @@
-https://github.com/0xJaredFromSubway/MEVBOT
-
-
-
-// From Bard
-    /*
-    pragma solidity ^0.8.0;
-
-    import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-    contract FrontRunnerBot {
-
-    address public owner;
-
-    ERC20 public token;
-
-    event FrontRun(
-        address indexed from,
-        address indexed to,
-        uint256 amount,
-        uint256 gasPrice,
-        uint256 slippage
-    );
-
-    constructor(address _token) {
-        owner = msg.sender;
-        token = ERC20(_token);
-    }
-
-    function frontRun() public {
-        // Loop through all pending transactions.
-        for (uint256 i = 0; i < web3.eth.pendingTransactions(0).length; i++) {
-        // Get the next transaction in the mempool.
-        bytes memory transaction = web3.eth.getTransaction(
-            web3.eth.getTransactionReceipt(
-            web3.eth.pendingTransactions(0)[i]
-            ).transactionHash
-        );
-
-        // Check if the transaction is vulnerable.
-        if (transaction.gasPrice < 10000000000 && transaction.slippage > 10) {
-            // Place two transactions to profit from the vulnerable transaction.
-            token.transfer(msg.sender, transaction.amount);
-            token.transfer(msg.sender, transaction.amount);
-
-            // Log the front run event.
-            emit FrontRun(
-            transaction.from,
-            msg.sender,
-            transaction.amount,
-            transaction.gasPrice,
-            transaction.slippage
-            );
-
-            // Break out of the loop since we found a suitable transaction.
-            break;
-        }
-        }
-    }
-
-    }
-    */
-
-
-// From Bard
-    /*
-    contract SandwichFrontrunner {
-
-    // The address of the token we are frontrunning
-    address public tokenAddress;
-
-    // The constructor sets the address of the token we are frontrunning
-    constructor(address _tokenAddress) {
-        tokenAddress = _tokenAddress;
-    }
-
-    // This function listens for pending transactions to the token contract
-    function listenForTransactions() public {
-        // Get all pending transactions to the token contract
-        bytes[] memory transactions = getPendingTransactions(tokenAddress);
-
-        // Loop through all pending transactions
-        for (uint i = 0; i < transactions.length; i++) {
-        // Get the transaction data
-        bytes memory transactionData = transactions[i];
-
-        // Check if the transaction is a buy transaction
-        if (transactionData.length == 96) {
-            // Get the amount of tokens being bought
-            uint amount = abi.decode(transactionData, (uint256));
-
-            // Calculate the profit we can make by sandwiching the transaction
-            uint profit = amount * 1000000000000000000 - amount;
-
-            // If the profit is greater than 0, we will sandwich the transaction
-            if (profit > 0) {
-            // Sell the tokens that are being bought
-            sellTokens(tokenAddress, amount);
-
-            // Buy the tokens back at a lower price
-            buyTokens(tokenAddress, amount);
-            }
-        }
-        }
-    }
-
-    // This function sells the tokens that are being bought by the frontrunning bot
-    function sellTokens(address _tokenAddress, uint _amount) public {
-        // Create a new transaction to sell the tokens
-        Transaction memory transaction = new Transaction(
-        _tokenAddress,
-        msg.sender,
-        _amount,
-        90000,
-        now + 1000
-        );
-
-        // Send the transaction
-        sendTransaction(transaction);
-    }
-
-    // This function buys the tokens back at a lower price
-    function buyTokens(address _tokenAddress, uint _amount) public {
-        // Create a new transaction to buy the tokens
-        Transaction memory transaction = new Transaction(
-        msg.sender,
-        _tokenAddress,
-        _amount,
-        90000,
-        now + 1000
-        );
-
-        // Send the transaction
-        sendTransaction(transaction);
-    }
-
-    // This function sends a transaction
-    function sendTransaction(Transaction memory _transaction) public {
-        // Get the transaction nonce
-        uint nonce = getNextNonce();
-
-        // Set the transaction nonce
-        _transaction.nonce = nonce;
-
-        // Sign the transaction
-        bytes memory signature = sign(_transaction);
-
-        // Send the transaction
-        send(_transaction, signature);
-    }
-
-    // This function gets the next nonce
-    function getNextNonce() public view returns (uint) {
-        // Get the current nonce
-        uint nonce = address(this).nonce;
-
-        // Increment the nonce
-        nonce++;
-
-        // Return the incremented nonce
-        return nonce;
-    }
-
-    // This function signs a transaction
-    function sign(Transaction memory _transaction) public view returns (bytes memory) {
-        // Get the private key
-        bytes32 privatekey = sha3("my_private_key");
-
-        // Sign the transaction
-        bytes memory signature = web3.eth.sign(_transaction, privatekey);
-
-        // Return the signature
-        return signature;
-    }
-
-    // This function sends a transaction
-    function send(Transaction memory _transaction, bytes memory _signature) public {
-        // Get the transaction gas price
-        uint gasPrice = 90000;
-
-        // Get the transaction value
-        uint value = 0;
-
-        // Send the transaction
-        web3.eth.sendTransaction(_transaction, gasPrice, value, _signature);
-    }
-    }
-    */
-
-
-// From Bard
-    /*
-    pragma solidity ^0.8.0;
-
-    import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-    contract SandwichFrontRunner is ERC20 {
-
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
-
-    function sandwichFrontRun(address from, address to, uint256 amount) public {
-        // Get the current price of the token.
-        uint256 currentPrice = this.getPrice();
-
-        // Place a buy order for the amount of tokens being sent.
-        buy(from, amount, currentPrice);
-
-        // Wait for the buy order to be filled.
-        while (!isFilled(from, amount)) {
-        // Do nothing.
-        }
-
-        // Sell the tokens back to the original sender.
-        sell(from, amount, currentPrice);
-    }
-
-    function buy(address to, uint256 amount, uint256 price) public {
-        // Send the tokens to the bot.
-        transfer(to, amount);
-
-        // Wait for the transaction to be mined.
-        while (!isMined(to, amount)) {
-        // Do nothing.
-        }
-
-        // Pay the original sender for the tokens.
-        pay(to, price * amount);
-    }
-
-    function sell(address from, uint256 amount, uint256 price) public {
-        // Receive the tokens from the original sender.
-        transferFrom(from, amount);
-
-        // Wait for the transaction to be mined.
-        while (!isMined(from, amount)) {
-        // Do nothing.
-        }
-
-        // Pay the original sender for the tokens.
-        pay(from, price * amount);
-    }
-
-    function isFilled(address from, uint256 amount) public view returns (bool) {
-        // Get the current balance of the bot.
-        uint256 botBalance = balanceOf(address(this));
-
-        // Return true if the bot has enough tokens to cover the buy order.
-        return botBalance >= amount;
-    }
-
-    function isMined(address from, uint256 amount) public view returns (bool) {
-        // Get the transaction receipt for the buy order.
-        TransactionReceipt receipt = txReceipt(from, amount);
-
-        // Return true if the transaction has been mined.
-        return receipt != null;
-    }
-
-    function pay(address to, uint256 amount) public {
-        // Send the amount to the original sender.
-        to.transfer(amount);
-    }
-
-    function scanMempool() public view returns (bool) {
-        // Get the current transactions in the mempool.
-        Mempool mempool = getMempool();
-
-        // Loop through the transactions and find a vulnerable transaction.
-        for (uint256 i = 0; i < mempool.length; i++) {
-        // Get the transaction details.
-        TransactionDetails details = mempool[i];
-
-        // Check if the transaction has low gas and high slippage.
-        if (details.gasPrice < 10000000000 && details.slippage > 10) {
-            // The transaction is vulnerable.
-            return true;
-        }
-        }
-
-        // No vulnerable transactions found.
-        return false;
-    }
-
-    function placeTransactions() public {
-        // If there are no vulnerable transactions in the mempool, do nothing.
-        if (!scanMempool()) {
-        return;
-        }
-
-        // Get the vulnerable transaction.
-        TransactionDetails details = mempool[0];
-
-        // Place a buy order for the amount of tokens being sent.
-        buy(details.from, details.amount, details.price);
-
-        // Wait for the buy order to be filled.
-        while (!isFilled(details.from, details.amount)) {
-        // Do nothing.
-        }
-
-        // Sell the tokens back to the original sender.
-        sell(details.from, details.amount, details.price);
-    }
-    }
-    */
-
-
-
-// Bard
-    /*
+// From Bard:
+/*
     pragma solidity ^0.8.0;
 
     contract FrontRunner {
@@ -317,6 +9,9 @@ https://github.com/0xJaredFromSubway/MEVBOT
 
     // The amount of ether in the bot contract
     uint public etherBalance;
+
+    // The maximum amount of ether that the bot can lose
+    uint public maxLoss;
 
     // The function to scan the mempool for vulnerable transactions
     function scanMempool() public returns (bool success) {
@@ -333,12 +28,16 @@ https://github.com/0xJaredFromSubway/MEVBOT
         // Check if the transaction is vulnerable
         if (transaction.gasPrice < averageGasPrice() && transaction.slippage > averageSlippage()) {
 
+            // Check if the bot can afford to lose money on this transaction
+            if (maxLoss > transaction.value) {
+
             // Place two transactions to profit from the vulnerable transaction
             placeTransactionBefore(transaction);
             placeTransactionAfter(transaction);
 
             // Success!
             return true;
+            }
         }
         }
 
@@ -435,9 +134,161 @@ https://github.com/0xJaredFromSubway/MEVBOT
         return transactions;
     }
 
+    // The function to stop the bot
+    function stop() public {
+
+        // Set the max loss to 0
+        maxLoss = 0;
     }
-    */
+
+    }
+
+*/
 
 
 
+// Bard
+/*
+    pragma solidity ^0.8.0;
 
+    contract FrontRunner {
+
+    // The address of the bot contract
+    address public botAddress;
+
+    // The amount of ether in the bot contract
+    uint public etherBalance;
+
+    // The maximum amount of money that the bot can lose
+    uint public maxLoss;
+
+    // The function to scan the mempool for vulnerable transactions
+    function scanMempool() public returns (bool success) {
+
+        // Get the most recent batch of pending transactions
+        bytes[] memory transactions = getAllTransactions();
+
+        // Loop through the transactions
+        for (uint i = 0; i < transactions.length; i++) {
+
+        // Get the transaction details
+        bytes memory transaction = transactions[i];
+
+        // Check if the transaction is vulnerable
+        if (transaction.gasPrice < averageGasPrice() && transaction.slippage > averageSlippage()) {
+
+            // Check if the bot can afford to lose money on this transaction
+            if (maxLoss > transaction.value) {
+
+            // Place two transactions to profit from the vulnerable transaction
+            placeTransactionBefore(transaction);
+            placeTransactionAfter(transaction);
+
+            // Success!
+            return true;
+            }
+        }
+        }
+
+        // No vulnerable transactions found
+        return false;
+    }
+
+    // The function to place a transaction before a vulnerable transaction
+    function placeTransactionBefore(bytes memory transaction) public {
+
+        // Create a new transaction
+        Transaction memory newTransaction = new Transaction(
+        transaction.from,
+        transaction.to,
+        transaction.value,
+        transaction.gasPrice - 1,
+        transaction.slippage - 1
+        );
+
+        // Send the transaction
+        sendTransaction(newTransaction);
+    }
+
+    // The function to place a transaction after a vulnerable transaction
+    function placeTransactionAfter(bytes memory transaction) public {
+
+        // Create a new transaction
+        Transaction memory newTransaction = new Transaction(
+        transaction.to,
+        transaction.from,
+        transaction.value,
+        transaction.gasPrice + 1,
+        transaction.slippage + 1
+        );
+
+        // Send the transaction
+        sendTransaction(newTransaction);
+    }
+
+    // The function to send a transaction
+    function sendTransaction(Transaction memory transaction) public {
+
+        // Get the transaction hash
+        bytes32 transactionHash = transaction.hash();
+
+        // Wait for the transaction to be mined
+        while (!transaction.isMined()) {
+        // Wait for 1 block
+        block.wait(1);
+        }
+
+        // Success!
+        etherBalance += transaction.value;
+    }
+
+    // The function to get the average gas price
+    function averageGasPrice() public view returns (uint gasPrice) {
+
+        // Get the last 1000 transactions
+        bytes[] memory transactions = getAllTransactions();
+
+        // Calculate the average gas price
+        gasPrice = 0;
+        for (uint i = 0; i < transactions.length; i++) {
+        gasPrice += transactions[i].gasPrice;
+        }
+        gasPrice /= transactions.length;
+
+        return gasPrice;
+    }
+
+    // The function to get the average slippage
+    function averageSlippage() public view returns (uint slippage) {
+
+        // Get the last 1000 transactions
+        bytes[] memory transactions = getAllTransactions();
+
+        // Calculate the average slippage
+        slippage = 0;
+        for (uint i = 0; i < transactions.length; i++) {
+        slippage += transactions[i].slippage;
+        }
+        slippage /= transactions.length;
+
+        return slippage;
+    }
+
+    // The function to get all transactions
+    function getAllTransactions() public view returns (bytes[] memory transactions) {
+
+        // Get the list of all transactions
+        transactions = eth.getTransactionReceipts(new address[](0));
+
+        return transactions;
+    }
+
+    // The function to stop the bot
+    function stop() public {
+
+        // Set the max loss to 0
+        maxLoss = 0;
+    }
+
+    }
+*/
