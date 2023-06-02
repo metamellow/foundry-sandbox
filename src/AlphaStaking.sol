b3034ad9baa507b0264ed73b458370507668916e
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-06-02 22:10:44 +0700

    Update AlphaStaking.sol

--- src/AlphaStaking.sol
+++ src/AlphaStaking.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-. */
-/* -.-.-.-.-.-. $CHADGPT ALPHA STAKING POOL .-.-.-.-.-. */
+/* -.-.-.-.-.-.-. ALPHA STAKING POOL v0.3 .-.-.-.-.-.-. */
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-. */
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
