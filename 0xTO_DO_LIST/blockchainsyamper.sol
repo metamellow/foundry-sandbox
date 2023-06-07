c9022fd3367dde588379667bb36c5445795e7ff9
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-06-07 12:57:57 +0700

    Update blockchainsyamper.sol

--- 0xTO_DO_LIST/blockchainsyamper.sol
+++ 0xTO_DO_LIST/blockchainsyamper.sol
@@ -2,3 +2,8 @@ some way for people to store ideas on chain easily
 would be great if they can do images
 or even agreements between two parties
 ans BON wouod take a tiny percentage
+
+
+
+
+https://blog.logrocket.com/decentralized-data-storage-using-ipfs-and-react-a-tutorial-with-examples/#how-to-build-a-frontend-dapp-with-ipfs-and-react
