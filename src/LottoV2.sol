fe8b78c3dbf58e95b92fa2c37f090fb3db780af8
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-07-04 04:29:11 +0700

    Update LottoV2.sol

--- src/LottoV2.sol
+++ src/LottoV2.sol
@@ -7,7 +7,8 @@ pragma solidity ^0.8.0;
 // --- contract ---
 // - ((1)) should split off 1% merp and 1% soc
 // - ((2)) should add a qrng results log
-// - ((3)) should add an easy mint NFT receipt to winner using an onlyOwner Transferfrom NFT id to claim
+// - ((3)) should add an easy mint NFT receipt towinner using an onlyOwner Transferfrom NFT id to claim
+// - ((4)) should deploy main net with tresor
 // --- website ---
 // - should add some cool wallet reading features on site with ethersJS
 // - should display last rounds results above the current rounds options; like darts or
