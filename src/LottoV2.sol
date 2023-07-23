d523efecb76936f3d70d60d9f5dd3e6c079248f3
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-07-23 20:11:27 +0700

    Update LottoV2.sol

--- src/LottoV2.sol
+++ src/LottoV2.sol
@@ -18,7 +18,7 @@ npx @api3/airnode-admin derive-sponsor-wallet-address \
 npx @api3/airnode-admin derive-sponsor-wallet-address \
 --airnode-xpub xpub6DXSDTZBd4aPVXnv6Q3SmnGUweFv6j24SK77W4qrSFuhGgi666awUiXakjXruUSCDQhhctVG7AQt67gMdaRAsDnDXv23bBRKsMWvRzo6kbf \
 --airnode-address 0x9d3C147cA16DB954873A498e0af5852AB39139f2 \
---sponsor-address 0x14b43f1b22b47c401dec863883b32e715313061e
+--sponsor-address 0x14d1e21657557ac43a94f2ffbd104c8dadce1b9a
 
 // Poly Mumbai
 npx @api3/airnode-admin derive-sponsor-wallet-address \
