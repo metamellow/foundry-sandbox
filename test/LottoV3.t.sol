c2dc6c26c1b34b4b3d60e4cce0f4fb59fdd38254
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-07-28 16:35:40 +0700

    testing continued

--- test/LottoV3.t.sol
+++ test/LottoV3.t.sol
@@ -4,25 +4,30 @@ pragma solidity ^0.8.17;
 /*
 NOTES:
 - test needs to be run via Alchemy rpc:
-    "forge test --fork-url https://polygon-mainnet.g.alchemy.com/v2/elpiyNU3HOchYaeMMpCteXolAFqJYTEi -vvv"
+forge test --fork-url https://polygon-mainnet.g.alchemy.com/v2/v4B-uiSecIHqGvzHRN21NJaX1Z87jtli -vvv
+-
+
 */
 
 import "forge-std/Test.sol";
 import "../src/LottoV3.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract contractTest is Test {
     LottoV3 public LottoV3Contract;
     address public ERC20token = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F; //USDT
+    address public user1 = address(69);
+    address public user2 = address(70);
+    address public user3 = address(71);
 
     function setUp() public{
         // --- WALLETS ---
-        address user = address(69);
-        vm.startPrank(user);
+        vm.startPrank(user1);
 
         // --- TOKENS ---
-        vm.deal(user, 1_000_000 ether);
+        vm.deal(user1, 1_000_000 ether);
 
-        deal(ERC20token, user, 1_000_000_000_069 ether);
+        deal(ERC20token, user1, 1_000_000_000_069 ether);
         require(IERC20(ERC20token).approve(
             0x8fA079a96cE08F6E8A53c1C00566c434b248BFC4, 
             115792089237316195423570985008687907853269984665640564039457584007913129639935), 
@@ -40,4 +45,32 @@ contract contractTest is Test {
         );
     }
 
+    function consoleLogs() public view{
+        console.log("_____________________CONTRT_INFOM_____________________");
+        console.log("CNCT ADDR: ", address(LottoV3Contract));
+        console.log("CNCT CNTR: ", LottoV3Contract.counter());
+
+        console.log("_____________________WALLET_INFOM_____________________");
+        console.log("USR1 GASB: ", address(user1).balance);
+        console.log("USR1 ERCB: ", ERC20(ERC20token).balanceOf(address(user1)));
+
+    }
+
+    function test_0_ConsoleLogs() public view{
+        consoleLogs();
+    }
+
+    function test_1_Bet() public {
+        vm.startPrank(user1);
+        LottoV3Contract.bet();
+        consoleLogs();
+        vm.startPrank(user2);
+        LottoV3Contract.bet();
+        consoleLogs();
+
+        address results;
+        results = LottoV3Contract.pastLottoResults(1);
+        console.log("RESULTS: ", results);
+
+    }
 }
