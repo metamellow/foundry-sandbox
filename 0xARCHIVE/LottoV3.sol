b1f3f9c30418332018223c7ce272916f08fd4cd5
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-09-01 15:43:56 +0700

    Update LottoV3.sol

--- 0xARCHIVE/LottoV3.sol
+++ 0xARCHIVE/LottoV3.sol
@@ -8,6 +8,37 @@ pragma solidity ^0.8.0;
 
 /* PS. We love you all! Stay happy, healthy, and wealthy!*/
 
+
+
+
+
+
+
+
+
+
+
+
+// NOTE make sure to edit the NFT collection name before delpoying
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
